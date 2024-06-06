58d6bbc55d2d5a10d021cb6223d06ec8df18e872
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-06-06 12:38:19 +0600

    draw canvas

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-void main()=>runApp(myApp());
+void main()=>runApp(const myApp());
 
 class myApp extends StatelessWidget {
   const myApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: DrawingBoard(),
     );
@@ -22,6 +24,8 @@ class DrawingBoard extends StatefulWidget {
 
 class _DrawingBoardState extends State<DrawingBoard> {
   Color selectedColor = Colors.black;
+  double strokeWidth = 5;
+  List<DrawingPoint> drawingPoints = [];
   List<Color> colors = [
     Colors.pink,
     Colors.red,
@@ -35,10 +39,48 @@ class _DrawingBoardState extends State<DrawingBoard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: GestureDetector(
+        onPanStart: (details){
+         setState(() {
+           drawingPoints.add(
+               DrawingPoint(details.localPosition,
+                   Paint()..color = selectedColor
+                     ..isAntiAlias = true
+                     ..strokeWidth = strokeWidth
+                     ..strokeCap = StrokeCap.round   // change to square to see effect
+
+               )
+           );
+         });
+        },
+        onPanUpdate: (details){
+         setState(() {
+           drawingPoints.add(
+               DrawingPoint(details.localPosition,
+                   Paint()..color = selectedColor
+                     ..isAntiAlias = true
+                     ..strokeWidth = strokeWidth
+                     ..strokeCap = StrokeCap.round   // change to square to see effect
+
+               )
+           );
+         });
+        },
+        // onPanEnd: (details){
+        //   drawingPoints.add();
+        // },
+        child: CustomPaint(
+          painter: _DrawingPainter(drawingPoints: drawingPoints),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+      ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
           color: Colors.grey.shade300,
-          padding: EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: List.generate(colors.length, (index)=>_buildColorChoose(colors[index]),),
@@ -67,4 +109,39 @@ class _DrawingBoardState extends State<DrawingBoard> {
               ),
     );
   }
+}
+
+
+class _DrawingPainter extends CustomPainter{
+  final List<DrawingPoint> drawingPoints;
+  _DrawingPainter({required this.drawingPoints});
+  List<Offset> offsetList = [];
+  @override
+  void paint(Canvas canvas, Size size) {
+for(int i=0; i<drawingPoints.length; i++)
+  {
+    if(drawingPoints[i] != null && drawingPoints[i+1] != null)
+      {
+        canvas.drawLine(drawingPoints[i].offset, drawingPoints[i+1].offset, drawingPoints[i].paint);
+      }
+    else if(drawingPoints[i]!=null && drawingPoints[i+1] == null)
+      {
+        offsetList.clear();
+        offsetList.add(drawingPoints[i].offset);
+
+        canvas.drawPoints(PointMode.points, offsetList, drawingPoints[i].paint);
+      }
+  }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+
+}
+
+class DrawingPoint{
+  Offset offset;
+  Paint paint;
+
+  DrawingPoint(this.offset, this.paint);
 }
